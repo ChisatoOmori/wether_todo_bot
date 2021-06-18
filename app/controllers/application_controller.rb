8355dfd4b7baec16293af3d14c957c7d962e21ad
Chisato Omori <reusu121@gmail.com>
2021-06-18 15:22:27 +0900

    linebot setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
+require 'line/bot'
+
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  before_action :validate_signature
+
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = "067b34857437dea749da9e84cb3fb35b"
+      config.channel_token = "OI/bY7BVoXNM5oGzIAjv3ZwrGigx0Vhg0BvpYKJl5nYHj77nHrUsjeKYaOW2zVZ5vsGon82dM0UMsDqF3PJ+lH2LeZyq/Hdo3ZmbZspSlfXwgTN/SMqPu2vq3map57KqwWC8OAJouN+mZpfjhbwt7wdB04t89/1O/w1cDnyilFU="
+      # ローカルで動かすだけならベタ打ちでもOK
+      # config.channel_secret = "your channel secret"
+      # config.channel_token = "your channel token"
+    }
+  end
 end
+
--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,40 +1,26 @@
 class LinebotController < ApplicationController
-    require 'line/bot'  # gem 'line-bot-api'
-  
-    # callbackアクションのCSRFトークン認証を無効
-    protect_from_forgery :except => [:callback]
-  
-    def client
-      @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = "067b34857437dea749da9e84cb3fb35b"
-        config.channel_token = "OI/bY7BVoXNM5oGzIAjv3ZwrGigx0Vhg0BvpYKJl5nYHj77nHrUsjeKYaOW2zVZ5vsGon82dM0UMsDqF3PJ+lH2LeZyq/Hdo3ZmbZspSlfXwgTN/SMqPu2vq3map57KqwWC8OAJouN+mZpfjhbwt7wdB04t89/1O/w1cDnyilFU="
-      }
-    end
-  
-    def callback
-      body = request.body.read
-  
-      signature = request.env['HTTP_X_LINE_SIGNATURE']
-      unless client.validate_signature(body, signature)
-        head :bad_request
-      end
-  
-      events = client.parse_events_from(body)
-  
-      events.each { |event|
-        case event
-        when Line::Bot::Event::Message
-          case event.type
-          when Line::Bot::Event::MessageType::Text
-            message = {
-              type: 'text',
-              text: event.message['text']
-            }
-          end
+
+  def callback
+    body = request.body.read
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
         end
-      }
-  
-      head :ok
-    end
+      end
+    }
+    "OK"
   end
-  
+end
