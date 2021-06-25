8006ed7b36b4a3c6618f62013db22649175103e9
ajiki <longerashi@gmail.com>
2021-06-25 13:24:57 +0900

    commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,32 +5,19 @@ class LinebotController < ApplicationController
     require 'rexml/document'
   
     def callback
-        body = request.body.read
+      body = request.body.read
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        return head :bad_request
+      end
+      events = client.parse_events_from(body)
+      events.each { |event|
 
-        signature = request.env['HTTP_X_LINE_SIGNATURE']
-        unless client.validate_signature(body, signature)
-          error 400 do 'Bad Request' end
-        end
-      
-        events = client.parse_events_from(body)
-        events.each do |event|
-          case event
-          when Line::Bot::Event::Message
-            case event.type
-            when Line::Bot::Event::MessageType::Text
-              message = {
-                type: 'text',
-                text: event.message['text']
-              }
-              client.reply_message(event['replyToken'], message)
-            when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-              response = client.get_message_content(event.message['id'])
-              tf = Tempfile.open("content")
-              tf.write(response.body)
-            end
-          end
+      }
+      head :ok
     end
   
+    
     private
   
     def client
