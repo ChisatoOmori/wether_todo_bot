20e2b04dd68da45337934238d2408bc8cfcd2743
ajiki <longerashi@gmail.com>
2021-06-25 13:33:47 +0900

    commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,7 +12,17 @@ class LinebotController < ApplicationController
       end
       events = client.parse_events_from(body)
       events.each { |event|
-
+        case event
+            when Line::Bot::Event::Message
+              case event.type
+              when Line::Bot::Event::MessageType::Text
+                message = {
+                  type: 'text',
+                  text: event.message['text']
+                }
+                client.reply_message(event['replyToken'], message)
+            end
+        end
       }
       head :ok
     end
