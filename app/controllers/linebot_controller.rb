a475d46f7e9a87fc33f007f0c37a1f59b2976ef7
ajiki <longerashi@gmail.com>
2021-06-25 12:23:48 +0900

    commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -4,53 +4,36 @@ class LinebotController < ApplicationController
     require 'kconv'
     require 'rexml/document'
   
-    def callback
-      body = request.body.read
-      signature = request.env['HTTP_X_LINE_SIGNATURE']
-      unless client.validate_signature(body, signature)
-        return head :bad_request
-      end
-      events = client.parse_events_from(body)
-      events.each { |event|
-        case event
-          # メッセージが送信された場合の対応を入力
-  
-          # ユーザーからテキスト形式のメッセージが送られて来た場合の対応を入力
-  
-            when /.*(明日|あした).*/
-              # info[2]：明日の天気
-              per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
-              per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
-              per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
-              if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-                push =
-                  "テキスト\nテキスト\n降水確率\n   6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\nテキスト"
-              else
-                push =
-                  "テキスト"            end
-            # テキスト以外（画像等）のメッセージが送られた場合
-          else
-            push = "テキスト"
-          end
-          message = {
-            type: 'text',
-            text: push
-          }
-          client.reply_message(event['replyToken'], message)
-          # LINEお友達追された場合
-        when Line::Bot::Event::Follow
-          # 登録したユーザーのidをユーザーテーブルに格納
-          line_id = event['source']['userId']
-          User.create(line_id: line_id)
-          # LINEお友達解除された場合
-        when Line::Bot::Event::Unfollow
-          # お友達解除したユーザーのデータをユーザーテーブルから削除
-          line_id = event['source']['userId']
-          User.find_by(line_id: line_id).destroy
+    post '/callback' do
+        body = request.body.read
+      
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+          error 400 do 'Bad Request' end
         end
-      }
-      head :ok
-    end
+      
+        events = client.parse_events_from(body)
+        events.each do |event|
+          case event
+          when Line::Bot::Event::Message
+            case event.type
+            when Line::Bot::Event::MessageType::Text
+              message = {
+                type: 'text',
+                text: event.message['text']
+              }
+              client.reply_message(event['replyToken'], message)
+            when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+              response = client.get_message_content(event.message['id'])
+              tf = Tempfile.open("content")
+              tf.write(response.body)
+            end
+          end
+        end
+      
+        # Don't forget to return a successful response
+        "OK"
+      end
   
     private
   
