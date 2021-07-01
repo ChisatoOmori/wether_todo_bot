091a6a6d6bae495738333d02ecdc2a5190f49869
ajiki <longerashi@gmail.com>
2021-07-01 16:11:59 +0900

    model

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -49,14 +49,7 @@ class LinebotController < ApplicationController
           }
           client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video, Line::Bot::Event::MessageType::Text
-          line = line_id.all
-          line.each do |line|
-            message = { 
-              "type": "text",
-              "text": "line"
-            }
-            client.reply_message(event['replyToken'], message)
-            end
+
           message = {
             "type": "template",
             "altText": "位置検索中",
--- db/migrate/20210701070600_create_users.rb
+++ db/migrate/20210701070600_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :line_id, null: false 
+      t.string :line_id, null: false
+      t.string :latitude, null: false
+      t.string :longitude, null: false
 
       t.timestamps
     end
