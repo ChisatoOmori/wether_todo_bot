73f4e09d0fcc068fe92f9c21d0d1e457df4fbb80
ajiki <longerashi@gmail.com>
2021-07-05 12:22:15 +0900

    明日の予定で返されるテキストを変更した

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -88,7 +88,7 @@ class LinebotController < ApplicationController
             }
           when '明日の予定'
             now = Time.current
-            message = { type: 'text', text: Blog.find(params[now.tomorrow])}
+            message = { type: 'text', text: @blog.title}
             client.reply_message(event['replyToken'], message)
           end
           client.reply_message(event['replyToken'], message)
